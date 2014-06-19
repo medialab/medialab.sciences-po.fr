469320c4205dc745446327b543226c51baf80e82
Daniele Guido <gui.daniele@gmail.com>
2014-06-19 17:46:40 +0200

    images max height/width bug corrected in home page

--- wp-content/themes/medialab_v4/index.php
+++ wp-content/themes/medialab_v4/index.php
@@ -25,7 +25,7 @@ get_header(); ?>	<!-- Début du header -->
 				<?php $video = get_post_meta($post->ID, 'vimeo_id', true); /* la requête va chercher la vignette de la vidéo du MOOC */
 				 if (!$video) {
 				 	echo $link;
-				 	the_post_thumbnail("thumb-zone-1");
+				 	the_post_thumbnail(array(456,456));//"thumb-zone-1");
 				 	echo "</a>";
 				 } else echo '<iframe src="http://player.vimeo.com/video/'.$video.'" width="456px" height="343px" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'; ?>
 			</div>
@@ -40,7 +40,7 @@ get_header(); ?>	<!-- Début du header -->
 		<div class="zone-2"> <!-- Zone du deuxième post en haut à droite-->
 			<?php $link = '<a href="'.get_permalink().'" rel="bookmark" title="Permanent Link to '.get_the_title().'">'; ?>
 			<h2><?php echo $link; the_title(); ?></a></h2>
-			<div class="content"><?php echo $link; the_post_thumbnail("thumb-zone-2"); ?></a></div>
+			<div class="content"><?php echo $link; the_post_thumbnail(array(456,456)); ?></a></div>
 			
 			<h3><?php echo_shorten_excerpt(get_the_excerpt(), 200); ?></h3>
 		</div>
@@ -49,13 +49,14 @@ get_header(); ?>	<!-- Début du header -->
 
 		<?php wp_reset_query(); ?>
 		<!-- Level 3 zone -->
+		<div class="zone-3">&nbsp;</div><div class="zone-3">&nbsp;</div>
 		<?php query_posts( array('post_type' => array( 'blog', 'projets', 'publications'), 'category_name' => '3rd-level', 'showposts' => 2 ));
 		while ( have_posts() ) : the_post(); ?>
 		<div class="zone-3"> <!-- Zone du troisième post en bas à droite-->
 			<?php $link = '<a href="'.get_permalink().'" rel="bookmark" title="Permanent Link to '.get_the_title().'">'; ?>
 			<h2><?php echo $link; the_title(); ?></a></h2>
 			<div class="content">
-				<?php echo $link; the_post_thumbnail("thumb-zone-3-4"); ?></a>
+				<?php echo $link; the_post_thumbnail(array(215,215)); ?></a>
 			</div>
 			<h3><?php echo_shorten_excerpt(get_the_excerpt(), 100); ?></h3>
 
@@ -67,50 +68,6 @@ get_header(); ?>	<!-- Début du header -->
 
 	</div> <!-- Fin de la zone de Une -->
 	
-	<!--ANCIENNE UNE : À SUPPRIMER ??????????????????????????????????????????????????????????? -->
-	<div id="zone-edito-old" style="display:none">
-		<!-- Most recent/important zone -->
-		<?php query_posts( array ( 'category_name' => '1st-level', 'posts_per_page' => 1 ) );?>
-		<?php while ( have_posts() ) : the_post(); ?>
-		<div class="zone-1">
-			<?php the_post_thumbnail("thumb-zone-3-4"); ?>
-
-
-
-			<?php echo get_post_meta($post->ID, 'post_video', true); ?>
-			<div class="edito-title important">
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-				<h3><?php echo get_post_meta($post->ID, 'subtitle', true); ?></h3>
-			</div>
-		</div>
-		<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
-		<!-- Second zone -->
-		<?php query_posts( array ( 'category_name' => '2nd-level', 'posts_per_page' => 1 ) );?>
-		<?php while ( have_posts() ) : the_post(); ?>
-		<div class="zone-2">
-			<?php the_post_thumbnail(); ?>
-			<div class="edito-title less-important">
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-				<h3><?php echo get_post_meta($post->ID, 'subtitle', true); ?></h3>
-			</div>
-		</div>
-		<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
-		<!-- Level 3 zone -->
-		<?php query_posts( array ( 'category_name' => '3rd-level', 'posts_per_page' => 2 ) );?>
-		<?php while ( have_posts() ) : the_post(); ?>
-		<div class="zone-3"> 
-			<?php the_post_thumbnail(); ?>
-			<div class="edito-title less-important">
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-				<h3><?php echo get_post_meta($post->ID, 'subtitle', true); ?></h3>
-			</div>
-		</div>
-		<?php endwhile; ?>
-		<?php wp_reset_query(); ?>
-	</div>
-	<!-- FIN ANCIENNE UNE : À SUPPRIMER ??????????????????????????????????????????????????????????? -->
 
 	<div class="columns-container"> <!-- div des colonnes "Projets", "Blog", "Tools", "Publications" -->
 
