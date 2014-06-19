23c8387f3b80a13880bbc4c3bb00adf15a2c136c
Daniele Guido <gui.daniele@gmail.com>
2014-06-19 17:28:24 +0200

    added grunt task to publish less file

--- wp-content/themes/medialab_v4/header.php
+++ wp-content/themes/medialab_v4/header.php
@@ -66,10 +66,12 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/js/pin_like.js"></script>
 
 
-	<!-- Voici les fichiers less : -->
+	<!-- Voici les fichiers less : 
 	<link rel="stylesheet/less" href="<?php echo get_template_directory_uri(); ?>/style.less" type="text/css" media="screen" />
 	<script src="<?php echo get_template_directory_uri(); ?>/js/less-1.3.0.min.js" type="text/javascript"></script>
-
+	-->
+	<link rel="stylesheet/css" href="<?php echo get_template_directory_uri(); ?>/style.min.css" type="text/css" media="screen" />
+	
 	
 </head>
 <body>
