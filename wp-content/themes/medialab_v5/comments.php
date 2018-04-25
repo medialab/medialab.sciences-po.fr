53fb64d983a5ff266132e1809f0d3529f72d2647
Robin de Mourat <robin.demourat@gmail.com>
2018-04-25 15:59:43 +0200

    add titles to comments section

--- wp-content/themes/medialab_v5/comments.php
+++ wp-content/themes/medialab_v5/comments.php
@@ -23,13 +23,14 @@ if ( post_password_required() )
 
 <div id="comments" class="comments-area single-post">
 
+	<?php $locale = get_locale() ?>
+	<?php $in_english = $locale == 'en_US' ?>
+
 	<?php // You can start editing here -- including this comment! ?>
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
-			<?php /*
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentytwelve' ), number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			*/?> <hr/>
+			<?php print $in_english ? 'Comments': 'Commentaires' ?>
 		</h2>
 
 		<ol class="commentlist">
